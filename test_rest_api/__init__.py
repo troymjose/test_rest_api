867a6b3d3c306931457655d7ee421e0d5b428b0b
Troy M Jose <trjose@cisco.com>
2023-03-11 15:26:52 +0530

    root files

---
+++ test_rest_api/__init__.py
@@ -0,0 +1,4 @@
+from test_rest_api.testing.runner import runner
+from test_rest_api.testing.decorator import test
+from test_rest_api.rest_api.decorator import rest_api
+from test_rest_api.testing.bug import bug, BugException
---
+++ test_rest_api/__main__.py
@@ -0,0 +1,16 @@
+import sys
+from test_rest_api.testing.runner import runner
+from test_rest_api.utils.error_msg import ErrorMsg
+
+if __name__ == "__main__":
+    # Validate if the user provided minimum number of required params in the command
+    if len(sys.argv) < 3:
+        sys.exit(ErrorMsg.INVALID_COMMAND)
+    # Get the arguments
+    command, test_suite_path = sys.argv[1], sys.argv[2]
+    # Run command
+    if command == 'run':
+        # Run the test suite
+        runner.run(path=test_suite_path)
+    else:
+        sys.exit(ErrorMsg.INVALID_COMMAND)
