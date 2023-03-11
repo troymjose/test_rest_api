f63628b7bfdf4e7a74ef2254d0b948f5fb604819
Troy M Jose <trjose@cisco.com>
2023-03-11 16:29:07 +0530

    rest api decorator

---
+++ test_rest_api/rest_api/decorator.py
@@ -0,0 +1,44 @@
+from test_rest_api.utils.error_msg import ErrorMsg
+from test_rest_api.rest_api.rest_api import RestApi
+
+
+def rest_api(func):
+    """
+    Decorate your rest api functions.
+
+    Example:
+
+        @rest_api
+        def login_api(username: str, password: str):
+            return {
+                    url: "https://my_domain.com/login",
+                    parameters: {'param1': 'value1', 'param2': 'value2'},
+                    headers: {'content-type': 'application/json'},
+                    body: {'name':username,'pass':password}
+                    }
+    """
+
+    def inner(*args, **kwargs) -> RestApi:
+        # Get the function name
+        name = func.__name__
+        # Get the file path of the function
+        module = func.__module__
+        # Call the function to retrieve user specified rest api data/config
+        rest_api_config: dict = func(*args, **kwargs)
+        # Validate if the rest_api_config is a valid dictionary
+        if not isinstance(rest_api_config, dict):
+            raise Exception(f"""
+                            {name} ({module})
+                            {ErrorMsg.INVALID_REST_API}
+                            """)
+        try:
+            # Create & return RestApi object
+            return RestApi(**rest_api_config)
+        except Exception as e:
+            raise Exception(f"""
+                            {name} ({module})
+                            {str(e)}
+                            {ErrorMsg.INVALID_REST_API}
+                            """)
+
+    return inner
