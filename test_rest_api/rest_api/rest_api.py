f36055698beb038231aea6589e2ee6a1ab18274f
Troy M Jose <trjose@cisco.com>
2023-03-20 13:18:50 +0530

    c

--- test_rest_api/rest_api/rest_api.py
+++ test_rest_api/rest_api/rest_api.py
@@ -1,8 +1,20 @@
+import traceback
 from dataclasses import dataclass
 from aiohttp import ClientResponse
-from test_rest_api.utils.aiohttp_session import AioHttpSession
+from aiohttp.client_exceptions import ClientConnectorError, InvalidURL, ContentTypeError
 from test_rest_api.utils.error_msg import ErrorMsg
+from test_rest_api.utils.aiohttp_session import AioHttpSession
 from test_rest_api.rest_api.exception import RestApiSendException
+
+
+class RestApiMethod:
+    GET: str = 'get'
+    POST: str = 'post'
+    PUT: str = 'put'
+    PATCH: str = 'patch'
+    DELETE: str = 'delete'
+    HEAD: str = 'head'
+    OPTIONS: str = 'options'
 
 
 @dataclass(frozen=True)
@@ -15,11 +27,16 @@ class RestApiResponse:
 
 
 class RestApi:
-    def __init__(self, url: str, parameters: dict = {}, headers: list = {}, body: dict = {}):
+    def __init__(self, name: str, module: str, url: str, parameters: dict = {}, headers: list = {}, body: dict = {}):
+        # Rest api variables
         self.url = url
         self.parameters = parameters
         self.headers = headers
         self.body = body
+        # File variables
+        self.name = name
+        self.module = module
+        # Aiohttp variables
         self._session = AioHttpSession()
 
     async def send(self, method: str):
@@ -33,19 +50,33 @@ class RestApi:
             if method not in {'get', 'post', 'put', 'patch', 'delete', 'head', 'options'}:
                 raise Exception(ErrorMsg.INVALID_METHOD)
             # Send the request
-            return await getattr(self, method)()
-        except Exception as e:
-            raise RestApiSendException(parent_exception=str(e))
+            async with getattr(self._session, method)(url=self.url,
+                                                      params=self.parameters,
+                                                      headers=self.headers,
+                                                      data=self.body) as response:
+                return await self._create_response(response=response)
+        except ClientConnectorError as exc:
+            raise RestApiSendException(name=self.name, module=self.module, parent_exception=str(exc),
+                                       traceback=traceback.format_exc())
+        except InvalidURL as exc:
+            raise RestApiSendException(name=self.name, module=self.module, parent_exception='Invalid ULR',
+                                       traceback=traceback.format_exc())
+        except ContentTypeError as exc:
+            raise RestApiSendException(name=self.name, module=self.module, parent_exception=str(exc),
+                                       traceback=traceback.format_exc())
+        except Exception as exc:
+            raise RestApiSendException(name=self.name, module=self.module, parent_exception=str(exc),
+                                       traceback=traceback.format_exc())
 
-    async def create_response(self, response: ClientResponse) -> RestApiResponse:
+    async def _create_response(self, response: ClientResponse) -> RestApiResponse:
         """
-        Create response object from aiohttp response
+        Create response instance object from aiohttp async response
         """
-        # Retrieve the response body
+        # Retrieve the response body in JSON
         body = await response.json()
         # Get the other response parameters
         status_code, headers, content_type = response.status, dict(response.headers), response.content_type
-        # Return the RestApiResponse instance object
+        # Create and return the RestApiResponse instance object
         return RestApiResponse(status_code=status_code,
                                content_type=content_type,
                                body=body,
@@ -53,50 +84,43 @@ class RestApi:
                                obj=response)
 
     async def get(self):
-        async with self._session.get(url=self.url,
-                                     params=self.parameters,
-                                     headers=self.headers,
-                                     data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP GET Request
+        """
+        return await self.send(method=RestApiMethod.GET)
 
     async def post(self):
-        async with self._session.post(url=self.url,
-                                      params=self.parameters,
-                                      headers=self.headers,
-                                      data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP POST Request
+        """
+        return await self.send(method=RestApiMethod.POST)
 
     async def put(self):
-        async with self._session.put(url=self.url,
-                                     params=self.parameters,
-                                     headers=self.headers,
-                                     data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP PUT Request
+        """
+        return await self.send(method=RestApiMethod.PUT)
 
     async def patch(self):
-        async with self._session.patch(url=self.url,
-                                       params=self.parameters,
-                                       headers=self.headers,
-                                       data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP PATCH Request
+        """
+        return await self.send(method=RestApiMethod.PATCH)
 
     async def delete(self):
-        async with self._session.put(url=self.url,
-                                     params=self.parameters,
-                                     headers=self.headers,
-                                     data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP DELETE Request
+        """
+        return await self.send(method=RestApiMethod.DELETE)
 
     async def head(self):
-        async with self._session.head(url=self.url,
-                                      params=self.parameters,
-                                      headers=self.headers,
-                                      data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP HEAD Request
+        """
+        return await self.send(method=RestApiMethod.HEAD)
 
     async def options(self):
-        async with self._session.options(url=self.url,
-                                         params=self.parameters,
-                                         headers=self.headers,
-                                         data=self.body) as response:
-            return await self.create_response(response=response)
+        """
+        Send HTTP OPTIONS Request
+        """
+        return await self.send(method=RestApiMethod.OPTIONS)
