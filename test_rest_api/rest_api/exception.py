0110801532c835713a8ecf4e65798c2db7ed2f21
Troy M Jose <trjose@cisco.com>
2023-03-30 15:45:19 +0530

    s

--- test_rest_api/rest_api/exception.py
+++ test_rest_api/rest_api/exception.py
@@ -1,81 +1,56 @@
-import re
-from test_rest_api.utils.error_msg import ErrorMsg
+from test_rest_api.utils.exception import TestRestApiException
 
 
-class RestApiException(Exception):
+class RestApiCreationException(TestRestApiException):
     """
-    Base Exception class
-    """
-    _no_data_to_display = 'No data to display'
-
-    @staticmethod
-    def format_traceback(traceback: str, search: str, extra_len: int) -> str:
-        """
-        Remove the data provided above the rest api file, to make the traceback more readable
-        """
-        if not traceback:
-            return ''
-        res = re.search(search, traceback)
-        if not res:
-            return traceback
-        return traceback[res.start() + extra_len:]
-
-
-class RestApiCreationException(RestApiException):
-    """
-    Exception raised while creating rest api object form json data returned form @rest_api decorated functions
+    Exception raised while creating rest api object using RestApi class
     """
 
-    def __init__(self, *, name: str, module: str, parent_exception: str, traceback: str):
-        self.message = f"""
-EXCEPTION
----------
-{parent_exception.strip() if parent_exception else self._no_data_to_display}
+    def __init__(self, *, msg: str):
+        self.exc = msg
+        self.error_msg = """
+Rest api creation failed
+Tip: Refer the example below !
 
-FILE NAME
----------
-{name.strip() if name else self._no_data_to_display}
+Example Code
+from test_rest_api import RestApi
+my_api = RestApi(
+                url="https://www.MyDomain.com/",
+                parameters={"param1":"val1","param2":"val2"},
+                headers={"Content-Type": "application/json"},
+                body={}
+                )
 
-FILE PATH
----------
-{module.strip() if module else self._no_data_to_display}
-
-TRACEBACK
----------
-{self.format_traceback(traceback=traceback, search=r'test_rest_api/test_rest_api/rest_api/decorator.py", line (.*?), in inner', extra_len=122).strip() if traceback else self._no_data_to_display}
-
-ERROR MESSAGE
--------------
-{ErrorMsg.INVALID_REST_API.strip()}
+Note: Only "url" is mandatory and rest are optional
 """
+        self.message = self.format(exc=self.exc, error_msg=self.error_msg)
         super().__init__(self.message)
 
 
-class RestApiSendException(RestApiException):
+class RestApiSendException(TestRestApiException):
     """
     Exception raised while sending rest api using aiohttp
     """
 
-    def __init__(self, *, name: str, module: str, parent_exception: str, traceback: str):
-        self.message = f"""
-EXCEPTION
----------
-{parent_exception.strip() if parent_exception else self._no_data_to_display}
+    def __init__(self, *, msg: str):
+        self.exc = msg
+        self.error_msg = f"""
+Rest api request failed
+Tip: Refer the example below !
 
-FILE NAME
----------
-{name.strip() if name else self._no_data_to_display}
+Example Code (All the below code does the same functionality but different with syntax):
 
-FILE PATH
----------
-{module.strip() if module else self._no_data_to_display}
+from my_api_file import my_api
+response = await my_api.send(method='get')
+or
+from test_rest_api import RestApi
+from my_api_file import my_api
+response = await my_api.send(method=RestApi.METHOD.GET)
+or
+from my_api_file import my_api
+response = await my_api.get()
 
-TRACEBACK
----------
-{self.format_traceback(traceback=traceback, search=r'test_rest_api/test_rest_api/rest_api/rest_api.py", line (.*?), in', extra_len=130).strip() if traceback else self._no_data_to_display}
-
-ERROR MESSAGE
--------------
-{ErrorMsg.REST_API_SEND_EXCEPTION.strip()}
+Supported methods: 'get', 'post', 'put', 'patch', 'delete', 'head', 'options'
 """
+        self.message = self.format(exc=self.exc, error_msg=self.error_msg)
         super().__init__(self.message)
