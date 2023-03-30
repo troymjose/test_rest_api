9de1a64aa66e45cc7826e60659fe65c2eebfff9e
Troy M Jose <trjose@cisco.com>
2023-03-30 15:24:30 +0530

    c

--- test_rest_api/global_variables/exception.py
+++ test_rest_api/global_variables/exception.py
@@ -1,34 +1,25 @@
-class VariableNotFoundException(Exception):
+from test_rest_api.utils.exception import TestRestApiException
+
+
+class VariableNotFoundException(TestRestApiException):
     """
     Exception raised for error in getattr(self, variable)
     """
 
     def __init__(self, *, name):
-        self.message = f"""
-EXCEPTION
----------
-Global variable not found
-
-ERROR MESSAGE
--------------
-'{name}' is not present in global variables
-"""
+        self.exc = 'Global variable not found'
+        self.error_msg = f'"{name}" is not present in global variables'
+        self.message = self.format(exc=self.exc, error_msg=self.error_msg)
         super().__init__(self.message)
 
 
-class ConstantSetException(Exception):
+class ConstantSetException(TestRestApiException):
     """
     Exception raised for error in updating variable instance object value with 'is_constant' flag as True
     """
 
     def __init__(self, *, name):
-        self.message = f"""
-EXCEPTION
----------
-Constant global variables cannot be updated
-
-ERROR MESSAGE
--------------
-'{name}' is a constant global variable
-"""
+        self.exc = 'Constant global variables cannot be updated'
+        self.error_msg = f'"{name}" is a constant global variable'
+        self.message = self.format(exc=self.exc, error_msg=self.error_msg)
         super().__init__(self.message)
