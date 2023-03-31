97b337c5075e5370fa3ed0d3a1f4c6ed96e8cabe
Troy M Jose <trjose@cisco.com>
2023-03-31 21:48:35 +0530

    s

--- test_rest_api/logger/exception.py
+++ test_rest_api/logger/exception.py
@@ -40,6 +40,16 @@ my_logger_messages = str(my_logger)
 Note: Both the above 2 examples does the same functionality but with different syntax
 
 It is recommended to return logger object for all you testcase functions, for rich html test reports
+
+(Example 3)
+
+from test_rest_api import test, Logger
+
+@test()
+def my_test():
+    my_logger = Logger()
+    my_logger.log(message = "log message 001")
+    return my_logger
 """
         self.message = self.format(exc=self.exc, error_msg=self.error_msg)
         super().__init__(self.message)
