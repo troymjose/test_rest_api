f1fd2cac7e54aa3d23644a92ba49e98eba8b40e2
Troy M Jose <trjose@cisco.com>
2023-04-26 00:08:42 +0530

    assert

--- test_rest_api/__init__.py
+++ test_rest_api/__init__.py
@@ -2,4 +2,5 @@ from .testing.bug import Bug
 from .testing.runner import runner
 from .testing.decorator import test
 from .rest_api.rest_api import RestApi
+from .assertion.assertion import Assert
 from .global_variables.global_variables import GlobalVariables
